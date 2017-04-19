130b6e6fcd886cf8bb68660ec9b6702a6b861167
Rafael Costa <rafacost@me.com>
2017-04-19 10:20:20 -0300

    Second commit

--- src/main/java/com/rafacost3d/bbs_mod/BBSMod.java
+++ src/main/java/com/rafacost3d/bbs_mod/BBSMod.java
@@ -1,14 +1,14 @@
-package com.example.examplemod;
+package com.rafacost3d.bbs_mod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = BBSMod.MODID, version = BBSMod.VERSION)
+public class BBSMod
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "bbs_mod";
     public static final String VERSION = "1.0";
     
     @EventHandler
