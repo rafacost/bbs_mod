f9e8dff542f2a51d1b3fe6bae49ac56319c643cb
Rafael Costa <rafacost@me.com>
2017-05-02 21:50:31 -0300

    Build.Gradle finally ok!

--- src/main/java/com/rafacost3d/bbs_mod/BBSMod.java
+++ src/main/java/com/rafacost3d/bbs_mod/BBSMod.java
@@ -5,11 +5,13 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = BBSMod.MODID, version = BBSMod.VERSION)
+@Mod(modid = BBSMod.MODID, name = BBSMod.MODNAME, version = BBSMod.VERSION, useMetadata = true)
+
 public class BBSMod
 {
     public static final String MODID = "bbs_mod";
-    public static final String VERSION = "1.0";
+    public static final String MODNAME = "Beer Brewery Simulator";
+    public static final String VERSION = "${version}";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
