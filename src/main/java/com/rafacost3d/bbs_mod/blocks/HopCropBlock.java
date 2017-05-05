15483c86e183e9ce2cd7d29b2a861dcdb153c87b
Rafael Costa <rafacost@me.com>
2017-05-05 12:34:12 -0300

    Build Alpha 01.15

--- src/main/java/com/rafacost3d/bbs_mod/blocks/HopCropBlock.java
+++ src/main/java/com/rafacost3d/bbs_mod/blocks/HopCropBlock.java
@@ -3,14 +3,23 @@ package com.rafacost3d.bbs_mod.blocks;
 import com.rafacost3d.bbs_mod.BBSItems;
 import com.rafacost3d.bbs_mod.BBSMod;
 import net.minecraft.block.BlockCrops;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
+import java.util.Random;
 
 public class HopCropBlock extends BlockCrops {
     private ResourceLocation nameBlock;
@@ -39,5 +48,44 @@ public class HopCropBlock extends BlockCrops {
     @SideOnly(Side.CLIENT)
     public void initModel() {
         ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(nameBlock, "inventory"));
+    }
+
+    @Override
+    public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+        List ret = super.getDrops(world, pos, state, fortune);
+        int age = this.getAge(state);
+        Random rand = world instanceof World ?((World)world).rand:new Random();
+        if(age >= this.getMaxAge()) {
+            int k = 3 + fortune;
+
+            for(int i = 0; i < k; ++i) {
+                if(rand.nextInt(2 * this.getMaxAge()) <= age) {
+                    ItemStack seeds = new ItemStack(this.getSeed(), 1, 0);
+
+                    if (haveQuality(seeds)) {
+                        getTagCompoundSafe(seeds).setInteger("Quality", getTagCompoundSafe(seeds).getInteger("Quality")+1);
+                    } else {
+                        getTagCompoundSafe(seeds).setInteger("Quality", 1);
+                        getTagCompoundSafe(seeds).setString("Country", "Domestic");
+                    }
+
+                    ret.add(seeds);
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    private NBTTagCompound getTagCompoundSafe(ItemStack stack) {
+        NBTTagCompound tagCompound = stack.getTagCompound();
+        if (tagCompound == null) {
+            tagCompound = new NBTTagCompound();
+            stack.setTagCompound(tagCompound);
+        }
+        return tagCompound;
+    }
+    private boolean haveQuality(ItemStack stack) {
+        return getTagCompoundSafe(stack).hasKey("Quality");
     }
 }
--- src/main/java/com/rafacost3d/bbs_mod/items/HopSeedsItem.java
+++ src/main/java/com/rafacost3d/bbs_mod/items/HopSeedsItem.java
@@ -9,10 +9,13 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -43,7 +46,6 @@ public class HopSeedsItem extends Item implements IPlantable {
         @SideOnly(Side.CLIENT)
         public void initModel() {
             ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
-            //player.sendMessage(new TextComponentString("Can't plant this seed on air..."));
         }
 
 
@@ -53,11 +55,25 @@ public class HopSeedsItem extends Item implements IPlantable {
         IBlockState state = worldIn.getBlockState(pos);
         if(facing == EnumFacing.UP && player.canPlayerEdit(pos.offset(facing), facing, itemstack) && state.getBlock().canSustainPlant(state, worldIn, pos, EnumFacing.UP, this) && worldIn.isAirBlock(pos.up())) {
             worldIn.setBlockState(pos.up(), this.crops.getDefaultState());
+
+
+
             itemstack.shrink(1);
             return EnumActionResult.SUCCESS;
         } else {
             return EnumActionResult.FAIL;
         }
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+        ItemStack itemstack = playerIn.getHeldItem(handIn);
+        if (haveQuality(itemstack)) {
+            playerIn.sendMessage(new TextComponentString("This hops seed is " + getTagCompoundSafe(itemstack).getString("Country") + ", with " + getTagCompoundSafe(itemstack).getInteger("Quality") + "% quality."));
+        } else {
+            playerIn.sendMessage(new TextComponentString("This seed is garbage..."));
+        }
+        return new ActionResult(EnumActionResult.PASS, playerIn.getHeldItem(handIn));
     }
 
     @Override
@@ -68,6 +84,18 @@ public class HopSeedsItem extends Item implements IPlantable {
     @Override
     public IBlockState getPlant(IBlockAccess world, BlockPos pos) {
         return this.crops.getDefaultState();
+    }
+
+    private NBTTagCompound getTagCompoundSafe(ItemStack stack) {
+        NBTTagCompound tagCompound = stack.getTagCompound();
+        if (tagCompound == null) {
+            tagCompound = new NBTTagCompound();
+            stack.setTagCompound(tagCompound);
+        }
+        return tagCompound;
+    }
+    private boolean haveQuality(ItemStack stack) {
+        return getTagCompoundSafe(stack).hasKey("Quality");
     }
 }
 
